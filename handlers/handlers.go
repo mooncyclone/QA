65b6e83a6baf60c7f2f124df52932e4edc347ce5
mooncyclone <nikita.pugachev@tages.ru>
2023-02-06 17:52:34 +0300

    new

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Router register necessary routes and returns an instance of a router.
-func Router() *mux.Router {
+func Router(string, string, string) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/home", home).Methods("GET")
 	return r
